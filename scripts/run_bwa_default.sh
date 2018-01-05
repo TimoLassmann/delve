f6199ec81d24e8f451326f3f807d1cc6be4ef884
Timo Lassmann <timolassmann@icloud.com>
2018-01-05 14:23:22 +0800

    cleaned up seed a little more..

---
+++ scripts/run_bwa_default.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#
+#	Set parameters below to fit your system
+#
+
+BWA_DIR="bwa"
+
+function run_aln () {
+    printf "Processing %s (bwa)" $file;
+    
+
+    $BWA_DIR aln -t 16 $GENOME_DIR   $file  | bwa samse $GENOME_DIR   - $file  -n 50   | samtools sort -@ 16 -O bam  -T ~/tmp - >  $file.bwa_default.bam
+
+    if [ "$?" -eq "0" ]
+    then
+	      printf "%30s\n" "Success";
+    else
+	      printf "\nERROR: bwa FAILED!\n\n";
+	      exit 1;
+    fi
+}
+
+GENOME=
+INDIR=
+
+function usage()
+{
+    cat <<EOF
+usage: $0  -g <genome file> -i <input dir>
+EOF
+    exit 1;
+}
+
+while getopts g:i: opt
+do
+    case ${opt} in
+	      g) GENOME_DIR=${OPTARG};;
+	      i) INDIR=${OPTARG};;
+	      *) usage;;
+    esac
+done
+
+if [ "${GENOME_DIR}" = "" ]; then usage; fi
+if [ "${INDIR}" = "" ]; then usage; fi
+
+for file in $INDIR/*
+do
+    if [ -f $file ]; then 
+	      if [[ $file =~ .f[q,a]$ ]]; then	    
+	          if [ -f $file.bwa_default.bam ]; then
+		            echo "$file already processed"
+	          else
+		            run_aln $file
+	          fi
+	      fi
+    fi
+done
